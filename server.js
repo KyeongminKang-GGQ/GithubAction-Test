9ee104039e47302374029f44a60cf5782bcf4e6c
KyeongminKang-GGQ <kyeongmin.kang@ggq.gg>
2022-11-07 09:58:41 +0900

    초기 설정

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).send({
+    version: process.env.VERSION,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
